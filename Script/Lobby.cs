19908091d52623ad076e3b502127549ef96170f3
Cokyz <36960437+Cokyz@users.noreply.github.com>
2019-06-13 16:55:52 +0700

    Add files via upload

---
+++ Script/Lobby.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class Lobby : MonoBehaviourPunCallbacks
+{
+    public static Lobby lobby;
+
+    public GameObject battleButton;
+    public GameObject cancleButton;
+
+    private void Awake()
+    {
+        lobby = this; //Create the singleton, live withing the main menu scene.
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();//Connects to master photon server
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Player has connected to the Photon Master Server");
+        battleButton.SetActive(true);// player now connected to servers, enables battlebutton to allow join a game
+    }
+
+    public void OnBattleButtonClicked()
+    {
+        battleButton.SetActive(false);
+        cancleButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();// join a rondom room
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tired to join a random game but failed. There must be no open games avaliable");
+        CreateRoom();
+    }
+
+    void CreateRoom()
+    {
+        int randomRoomName = Random.Range(0, 10000);
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 10 };
+        PhotonNetwork.CreateRoom("Room" + randomRoomName, roomOps);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tired to create a new room but failed, there must already be a room with the same name");
+        CreateRoom();
+    }
+
+    public void OnCancleButtonClicked()
+    {
+        cancleButton.SetActive(false);
+        battleButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Script/player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed;
+    public Vector2 jumpForce = new Vector2(0, 200);
+    Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            GetComponent<Rigidbody2D>().AddForce(jumpForce);
+        }*/
+    }
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2 (speed * move, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            GetComponent<Rigidbody2D>().AddForce(jumpForce);
+        }
+    }
+
+}
